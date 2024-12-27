5ebac539e3388b12150ef1735b865859c43b2a91
saathathm <saaadhath@gmail.com>
2024-12-27 14:47:46 +0530

    check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3030,
+  },
   build: {
     sourcemap: false, // Disable source maps to reduce memory usage
     rollupOptions: {
