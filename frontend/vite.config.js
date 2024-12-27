91bc966508da09b8653c7a384072407d142c95e5
saathathm <saaadhath@gmail.com>
2024-12-27 17:03:37 +0530

    corrections

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3030,
-  },
-  build: {
-    sourcemap: false, // Disable source maps to reduce memory usage
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom"], // Split vendor dependencies
-        },
-      },
-    },
-  },
-});
+})
